b66e19f93f651c387f8be036b8d803d49ddd4099
Ygor Frazão <ygorluizfrazao@gmail.com>
2023-03-30 21:26:06 -0300

    Update

--- audio-services/src/main/java/br/com/frazo/audio_services/player/AudioPlayingData.kt
+++ audio-services/src/main/java/br/com/frazo/audio_services/player/AudioPlayingData.kt
@@ -1,7 +1,6 @@
 package br.com.frazo.audio_services.player
 
 data class AudioPlayingData(val status: AudioPlayerStatus, val duration: Long, val elapsed: Long)
-
 enum class AudioPlayerStatus{
     NOT_INITIALIZED, PLAYING, PAUSED
 }
